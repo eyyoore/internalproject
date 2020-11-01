8f139cacefcc4c8411993fd952c6e9640c443dc0
eyyoore <73768500+eyyoore@users.noreply.github.com>
2020-11-01 14:49:23 +0700

    Update encfile.py

--- encfile.py
+++ encfile.py
@@ -3,3 +3,5 @@ import crypt
 filename = '' #inputfilename
 key = '' #inputkey
 print(crypt.crypt(filename, key))
+
+#to make sure the key is right try to input 'test' as a filename. The actual hash will be : o0uim5lPK3G0A
